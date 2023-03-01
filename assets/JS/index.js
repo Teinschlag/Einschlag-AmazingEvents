0a656cf401cb431d023a98dc6dc5d3bed947bbbd
Tomi <tomi94.lp@gmail.com>
2023-03-01 20:27:46 -0300

    Segui ordenando checkboxes, estilos y demas

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -8,7 +8,7 @@ for(let element of data.events){
     div.style.width = "18rem"
     div.innerHTML = `<img src="${element.image}" class="card-img-top" style="height: 150px" alt="Cinema">
     <div class="card-body d-flex flex-column justify-content-between">
-      <h5 class="card-title">${element.name}</h5>
+      <h3 class="card-title">${element.name}</h3>
       <p class="card-text">${element.description}</p>
       <p>Price: ${element.price} u$d</p>
       <a href="./details.html" class="btn btn-dark nav-item p-2 me-1 ms-1 mb-1"
@@ -44,7 +44,7 @@ let fragmentCheck = document.createDocumentFragment()
  for(let cat of result){
      let div = document.createElement('div')
      div.className="form-check form-check-inline"
-     div.style.color="#"
+     div.style.color="#d63384"
      div.innerHTML = `<input class="form-check-input" type="checkbox" id=${cat.split(' ').join('_')} value=${cat}>${cat}
      <label class="form-check-label" style="color: #d63384" for=${cat.split(' ').join('_')}></label>`
          fragmentCheck.appendChild(div)
