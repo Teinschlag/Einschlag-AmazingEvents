6df40af6670ac6d481058c4dcf3a2a97f922cb05
Tomi <tomi94.lp@gmail.com>
2023-03-07 00:10:34 -0300

    Filtran los checkbox

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -30,7 +30,6 @@ function checkbox(array) {
 
     for (let elemento of array){
         let categories = elemento.category
-        console.log(categories)
         arrayCat.push(categories)
     }
 
@@ -45,7 +44,8 @@ let fragmentCheck = document.createDocumentFragment()
      let div = document.createElement('div')
      div.className="form-check form-check-inline"
      div.style.color="#d63384"
-     div.innerHTML = `<input class="form-check-input" type="checkbox" id=${cat.split(' ').join('_')} value=${cat}>${cat}
+     div.innerHTML = `<input class="form-check-input" type="checkbox" id=${cat.split(' ').join('_')} value=${cat.split(' ').join('_')}>
+     ${cat}
      <label class="form-check-label" style="color: #d63384" for=${cat.split(' ').join('_')}></label>`
          fragmentCheck.appendChild(div)
  }
@@ -53,3 +53,70 @@ let fragmentCheck = document.createDocumentFragment()
  }
 
 check_category.appendChild(checkbox(data.events))
+
+///////////////////////////////////////////////////////////////////////////
+
+
+check_category.addEventListener('click', imprimirPorConsola)
+    
+function imprimirPorConsola(e){
+     console.log(e.target)
+ }
+
+ const checkboxes = document.querySelectorAll('input[type=checkbox]')
+ console.log(checkboxes)
+
+ checkboxes.forEach(checkbox => {
+     checkbox.addEventListener('change', verSeleccion)
+ })
+
+ // ver inputs chequeados
+ function verSeleccion(){
+     let inputsChequeados= Array.from(checkboxes).filter(checkbox => checkbox.checked)
+     let inputValues = inputsChequeados.map(input => input.value)
+     console.log(inputValues)
+     
+
+     let eventosCheck = data.events.filter(objeto => inputValues.includes(objeto.category.split(' ').join('_')))    
+        console.log(eventosCheck)
+     
+        // let newFragment = document.createDocumentFragment()
+
+        // for(let objeto of eventosCheck){
+        //     let div = document.createElement('div')
+        //     div.classList.add("card")
+        //     div.style.width = "18rem"
+        //     div.innerHTML = `<img src="${objeto.image}" class="card-img-top" style="height: 150px" alt="Cinema">
+        //     <div class="card-body d-flex flex-column justify-content-between">
+        //       <h3 class="card-title">${objeto.name}</h3>
+        //       <p class="card-text">${objeto.description}</p>
+        //       <p>Price: ${objeto.price} u$d</p>
+        //       <a href="./details.html" class="btn btn-dark nav-item p-2 me-1 ms-1 mb-1"
+        //         style="color: #d63384; background-color: black">More</a>
+        //     </div>`
+        //     newFragment.appendChild(div)
+        // }
+        // card_index.appendChild(newFragment)
+
+        // SE CREAN LAS TARJETAS, PERO SI SELECCIONO OTRO CHECKBOX ADEMAS, SE DUPLICAN, Y ADEMAS NO SE BORRAN LAS QUE NO CUMPLEN LA 
+        //CONDICION. A REVISAR!
+     
+ }
+
+ const formulario = document.getElementById('form1')
+
+ const filtrar = () => {
+    const texto = form1.value.toLowerCase();
+    for(let name of data.events){
+        let name = data.events.name.toLowerCase();
+    }
+    console.log("Se ve el filtrado")
+ }
+
+
+
+ 
+
+
+ 
+
